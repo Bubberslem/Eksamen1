c37c7b80f378f7c67dfb4bce5be778cfda47bbd4
Bubberslem <Rasmussenm0@gmail.com>
2025-01-05 19:46:11 +0100

    Indkøbskurv

--- src/IndkøbsKurven/Main.java
+++ src/IndkøbsKurven/Main.java
@@ -7,13 +7,16 @@ public class Main {
     public static void main(String[] args) {
         Basket basket = new Basket();
 
-        ArrayList<Product> products = new ArrayList<>();
+        ArrayList<Clothes> clothes = new ArrayList<>();
+        ArrayList<Electronics> electronics = new ArrayList<>();
+        ArrayList<Food> foods = new ArrayList<>();
+
         // Add products
-        products.add(new Clothes("T-shirt", 49.85, "10", "black"));
-        products.add(new Clothes("Pants", 88.00, "8", "brown"));
-        products.add(new Electronics("Headset", 100.00, 6));
-        products.add(new Electronics("Tablet", 255.00, 24));
-        products.add(new Food("Milk", 1.75, "1-6-2025"));
+        clothes.add(new Clothes("T-shirt", 49.85, "10", "black"));
+        clothes.add(new Clothes("Pants", 88.00, "8", "brown"));
+        electronics.add(new Electronics("Headset", 100.00, 6));
+        electronics.add(new Electronics("Tablet", 255.00, 24));
+        foods.add(new Food("Milk", 1.75, "1-6-2025"));
 
         Scanner scanner = new Scanner(System.in);
 
@@ -34,15 +37,13 @@ public class Main {
                 case 1:
                     // Show clothes
                     System.out.println("\nClothes:");
-                    for (int i = 0; i < products.size(); i++) {
-                        if (products.get(i) instanceof Clothes) {
-                            System.out.println((i + 1) + ". " + products.get(i));
-                        }
+                    for (int i = 0; i < clothes.size(); i++) {
+                        System.out.println((i + 1) + ". " + clothes.get(i));
                     }
                     System.out.print("Choose a product to add to basket (0 to cancel): ");
                     int clothesChoice = scanner.nextInt();
-                    if (clothesChoice > 0 && clothesChoice <= products.size() && products.get(clothesChoice - 1) instanceof Clothes) {
-                        basket.addProduct(products.get(clothesChoice - 1));
+                    if (clothesChoice > 0 && clothesChoice <= clothes.size()) {
+                        basket.addProduct(clothes.get(clothesChoice - 1));
                         System.out.println("Product added to basket.");
                     }
                     break;
@@ -50,15 +51,13 @@ public class Main {
                 case 2:
                     // Show electronics
                     System.out.println("\nElectronics:");
-                    for (int i = 0; i < products.size(); i++) {
-                        if (products.get(i) instanceof Electronics) {
-                            System.out.println((i + 1) + ". " + products.get(i));
-                        }
+                    for (int i = 0; i < electronics.size(); i++) {
+                        System.out.println((i + 1) + ". " + electronics.get(i));
                     }
                     System.out.print("Choose a product to add to basket (0 to cancel): ");
                     int electronicsChoice = scanner.nextInt();
-                    if (electronicsChoice > 0 && electronicsChoice <= products.size() && products.get(electronicsChoice - 1) instanceof Electronics) {
-                        basket.addProduct(products.get(electronicsChoice - 1));
+                    if (electronicsChoice > 0 && electronicsChoice <= electronics.size()) {
+                        basket.addProduct(electronics.get(electronicsChoice - 1));
                         System.out.println("Product added to basket.");
                     }
                     break;
@@ -66,15 +65,13 @@ public class Main {
                 case 3:
                     // Show food
                     System.out.println("\nFood:");
-                    for (int i = 0; i < products.size(); i++) {
-                        if (products.get(i) instanceof Food) {
-                            System.out.println((i + 1) + ". " + products.get(i));
-                        }
+                    for (int i = 0; i < foods.size(); i++) {
+                        System.out.println((i + 1) + ". " + foods.get(i));
                     }
                     System.out.print("Choose a product to add to basket (0 to cancel): ");
                     int foodChoice = scanner.nextInt();
-                    if (foodChoice > 0 && foodChoice <= products.size() && products.get(foodChoice - 1) instanceof Food) {
-                        basket.addProduct(products.get(foodChoice - 1));
+                    if (foodChoice > 0 && foodChoice <= foods.size()) {
+                        basket.addProduct(foods.get(foodChoice - 1));
                         System.out.println("Product added to basket.");
                     }
                     break;
